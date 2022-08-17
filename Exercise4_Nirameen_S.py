b2380e1f1eb7f2e4cbd9a6fb6768960ef5226405
mingnirmin <111176670+mingnirmin@users.noreply.github.com>
2022-08-17 17:00:25 +0700

    Add files via upload

---
+++ Exercise4_Nirameen_S.py
@@ -0,0 +1,9 @@
+FoundEng = 85
+GenBus = 79.9
+IntCom = 90.8
+ComPro = 88.7
+print("---Your Score---")
+print("Foundation English :","               ", FoundEng)
+print("General Business :","                 ", GenBus)
+print("Introduction to Computer Systems :"," ", IntCom)
+print("Computer Programming :","             ", ComPro)
